f5c424623133c40e9e8a0eefb5d3f7f2e8947cf9
Bruno <brubemal@gmail.com>
2024-10-01 22:02:05 -0300

    alocação e ponteiro

--- Untitled-1.c
+++ Untitled-1.c
@@ -14,34 +14,51 @@ que ocorreram
 
 void alocaInt(int **p, int tam);
 void alocaFloat(float **p,  int tam);
-void ReceberTemperaturas(int *p, int tam);
+void receberTemp(float *p, int tam);
+void calcularMedia(float *p, int tam, float *media);
+void leituraMedia(float media);
 
 int main()
 {
     int dias = 30;
-    float *temperaturas = NULL, Media = 0;
+    float *temperaturas = NULL, media= 0;
     alocaFloat(&temperaturas, dias);
-    ReceberTemperaturas(temperaturas, dias);
+    receberTemp(temperaturas, dias);
+    calcularMedia(temperaturas, dias, &media);
+    leituraMedia(media);
     return 0;
 }
-void alocaInt(int **p,int tam);
+void alocaInt(int **p,int tam)
 {
  *p = (int*)realloc(*p, tam *sizeof(int));
  if(*p == NULL){
     exit(1);
  }
 }
-void alocaFloat(int **p,int tam)
+void alocaFloat(float **p,int tam)
 {
-*p = (float*)realloc(*p, tam *sizeof(int));
+*p = (float*)realloc(*p, tam *sizeof(float));
 if(*p ==NULL){
     exit(1);
 }
 }
-void ReceberTemperaturas(float *p,int tam)
+void receberTemp(float *p,int tam)
 {
-    for(i = 0; i < tam; i++){
-        printf("\nTemperaturas: %f", i+1);
-        scanf("%f", (p+1));
+    for(int i = 0; i < tam; i++){
+        printf("\nTemperaturas %d: ", i+1);
+        scanf("%f", (p+i));
     }
+}
+void calcularMedia(float *p, int tam, float *media)
+{
+    for (int i = 0; i < tam; i++)
+    {
+        *media += *(p + i);
+    }
+    *media = *media / tam;
+    
+}
+void leituraMedia(float media)
+{
+    printf("A media das temperaturas \x82: %.2f", media);
 }
