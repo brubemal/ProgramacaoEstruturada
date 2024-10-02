1e8c8e870a3092fc8ef5243ee30c3aca141f5f0b
brubemal <brubemal@gmail.com>
2024-10-02 08:25:40 -0300

    Update Untitled-1.c

--- Untitled-1.c
+++ Untitled-1.c
@@ -17,15 +17,20 @@ void alocaFloat(float **p,  int tam);
 void receberTemp(float *p, int tam);
 void calcularMedia(float *p, int tam, float *media);
 void leituraMedia(float media);
+void calculaMaior(float *p, int tam, float *maiorTemp, int *diaMaior);
+void leituraMaiorTemp(float maiorTemp, int diaMaior);
 
 int main()
 {
-    int dias = 30;
-    float *temperaturas = NULL, media= 0;
+    int dias = 30, diaMaior = 0;
+    float *temperaturas = NULL, media= 0, maiorTemp = 0;
     alocaFloat(&temperaturas, dias);
     receberTemp(temperaturas, dias);
+    printf("\n\n");
     calcularMedia(temperaturas, dias, &media);
     leituraMedia(media);
+    calculaMaior(temperaturas, dias, &maiorTemp, &diaMaior);
+    leituraMaiorTemp(maiorTemp, diaMaior);
     return 0;
 }
 void alocaInt(int **p,int tam)
@@ -60,5 +65,20 @@ void calcularMedia(float *p, int tam, float *media)
 }
 void leituraMedia(float media)
 {
-    printf("A media das temperaturas \x82: %.2f", media);
+    printf("\nA media das temperaturas \x82: %.2f", media);
+}
+void calculaMaior(float *p, int tam, float *maiorTemp, int *diaMaior)
+{
+	for(int i = 0; i < tam; i++)
+	{
+	if(*(p + i) > *maiorTemp){
+		*maiorTemp = *(p + i);
+		*diaMaior = i + 1;
+	}
+	}
+	
+}
+void leituraMaiorTemp(float maiorTemp, int diaMaior)
+{
+	printf("\nA maior temp \x82 %.2f e o dia que ocorrou foi %i", maiorTemp, diaMaior);	
 }
